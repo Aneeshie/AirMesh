8a27a983fc9f836516f596e0ab4c243c31679ad1
Aneeshie <aneeshdas556@gmail.com>
2026-04-11 12:48:16 +0530

    feat: add basic file transfer sender with filename and size metadata

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -1,33 +1,60 @@
 package protocol
 
 import (
+	"bytes"
 	"encoding/binary"
+	"fmt"
 	"net"
+	"os"
+	"path/filepath"
 )
 
-func SendMessage(conn net.Conn, message string){
-	data := []byte(message)
+func SendMessage(conn net.Conn, path string) {
+	var buf bytes.Buffer
 
-	length:= uint32(len(data))
-	lengthBuf := make([]byte,4)
-	binary.BigEndian.PutUint32(lengthBuf, length)
+	baseName := filepath.Base(path)
 
-	conn.Write(lengthBuf)
-	conn.Write(data)
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Could not open file:", err)
+		return
+	}
+	defer file.Close()
+
+	n, err := buf.ReadFrom(file)
+	if err != nil {
+		fmt.Println("Could not read file:", err)
+		return
+	}
+
+	//filename length
+	nameBytes := []byte(baseName)
+	nameLenBuf := make([]byte, 8)
+	binary.BigEndian.PutUint64(nameLenBuf, uint64(len(nameBytes)))
+
+	//file size
+	sizeBuf := make([]byte, 8)
+	binary.BigEndian.PutUint64(sizeBuf, uint64(n))
+
+	conn.Write(nameLenBuf)
+	conn.Write(nameBytes)
+
+	conn.Write(sizeBuf)
+	conn.Write(buf.Bytes())
 }
 
-func ReadExact(conn net.Conn, size int) ([]byte, error){
+func ReadExact(conn net.Conn, size int) ([]byte, error) {
 	buffer := make([]byte, size)
-
 	total := 0
 
 	for total < size {
 		n, err := conn.Read(buffer)
 		if err != nil {
-			return nil,err
+			return nil, err
 		}
 
 		total += n
+
 	}
 
 	return buffer, nil
